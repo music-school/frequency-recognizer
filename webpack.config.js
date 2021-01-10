30e39f09a08d87395a84b65087e1a40a2ca03347
Aleksandr Grigorenko <techgriss@gmail.com>
2021-01-10 19:28:15 +0300

    frequency-recognizer v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "lib/index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    library: "frequencyRecognizer",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+	},
+	mode: "development",
+	resolve: {
+		fallback: {
+			fs: false
+		}
+	},
+	plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "external/aubio.wasm", to: "" },
+      ],
+    }),
+  ],
+}
